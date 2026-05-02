2a9727e93044d9ac3b716fc71cc1b44ca4189e45
Macjim202 <macjim202@gmail.com>
2026-05-02 23:36:45 +0500

    Task1

---
+++ Tyuiu.ZaitsevMS.Sprint6.Task1.V0.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.ZaitsevMS.Sprint6.Task1.V0.Lib;
+
+namespace Tyuiu.ZaitsevMS.Sprint6.Task1.V0.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double[] result = ds.GetMassFunction(1, 3);
+            double[] wait = { 0.84, 0.91, 0.14 };
+
+            CollectionAssert.AreEqual(wait, result);
+        }
+    }
+}
